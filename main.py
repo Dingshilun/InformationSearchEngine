e00062353fd272022ab2d9c936af4d28bbad2a02
DearRabbit <DearRabbit@users.noreply.github.com>
2017-06-21 21:31:04 +0800

    add argparser

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-from builder import *
-import builder
+#!/usr/bin/env python
+# coding: utf-8
 
-def train():
-    filepath="/Users/dingshilun/Documents/Senior/searchEngine/Reuters/*.html"
+import sys
+import builder
+import argparse
+
+def train(fn):
+    filepath="Reuters/*.html"
     a=builder.indexBuilder()
-    a.buildWordSet(filepath)
+    a.buildWordSet(fn)
     a.save()
 
 def init():
     a=builder.indexBuilder()
     a.load()
     return a
-indexFactory=init()
 
-print len(indexFactory.wordSet)
+def parse_main(argv):
+    parser = argparse.ArgumentParser(description='Search Engine')
+    parser.add_argument('-t', help='generate index and dictionary')
+    parser.add_argument('-q', help='query keys')
+    parser.add_argument('-k', type=int, help='Top K')
+    parser.add_argument('-V', '--vsm', action='store_true', default=True)
+    parser.add_argument('-B', '--bool', action='store_true')
+    parser.add_argument('-P', '--phrase', action='store_true')
+
+    args = parser.parse_args(argv)
+
+    if args.t:
+        train(args.t)
+    else:
+        indexFactory = init()
+        print len(indexFactory.wordSet)
+        if args.bool:
+            if args.q:
+                words = args.q
+                words = words.replace('(', ' ( ')
+                words = words.replace(')', ' ) ')
+                words = words.split()
+                print words
+            else:
+                print 'Missing query keywords'
+
+        elif args.phrase:
+            if args.q:
+                words = args.q.split()
+                print words
+            else:
+                print 'Missing query keywords'
+
+        elif args.vsm:
+            if args.q:
+                words = args.q.split()
+                print words
+            else:
+                print 'Missing query keywords'
+
+if __name__ == '__main__':
+    parse_main(sys.argv[1:])
